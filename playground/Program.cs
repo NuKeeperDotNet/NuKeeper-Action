31f10d8608003b32a2f807bbcf42e9c3a153b53e
Ivan Zub <ivan.zub@mendix.com>
2020-05-18 20:41:00 +0200

    Add sample project

---
+++ playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_Action_Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
